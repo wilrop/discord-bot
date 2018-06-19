21698d4bad2392774d913bd1750a3dab132f415f
Willem Röpke <willemropke@hotmail.com>
2018-06-20 01:30:20 +0200

    Added comments and a config file that now contains all the sensitive information.

--- Discord-bot.py
+++ Discord-bot.py
@@ -1,35 +1,41 @@
-"""
-Discord bot by Willem Röpke
+############################################
+#                                          #
+#               Willem Röpke               #
+#                20-06-2018                #
+#                                          #
+############################################
 
-"""
 
+import config                   # Importing the config file
 import discord                  # Importing the discord library
-import asyncio                  # I need coroutines
+import asyncio                  # Importing functionality for coroutines
 import random                   # For when we need random things
-import os                       # For when we need things revolving around the OS.
+import os                       # For when we need things revolving around the OS
 from requests import get        # Getting the quote
 from json import loads          # Getting info from json file
 
-token = 'NDU3MjkxODkxMjQ4MjAxNzM4.DgXC7g.iXM4gMDPGujH8eppMAkxLvVOSZE'    # Secret token for the bot
+token = config.token    # Secret token for the bot
 
 ## The discord client itself
 client = discord.Client()  
 
-
+# This procedure handles the hello message
 def handleHello(message):
-    msg = 'Hello {0.author.mention}'.format(message)
+    msg = 'Hello {0.author.mention}'.format(message) # Reply with the name of the author of the message
     return msg
 
+# This procedure handles the quote message
 def handleQuote():
-    response = get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
-    msg = '{quoteText} - {quoteAuthor}'.format(**loads(response.text))
+    response = get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')  # Get a quote from an api
+    msg = '{quoteText} - {quoteAuthor}'.format(**loads(response.text))  # Fomat the quote
     return msg
 
+# This procedure handles the meme message
 def handleMeme():
-    path = r'D:\Documenten\Programmeren\Discord bot\Memes'
-    random_filename = random.choice([x for x in os.listdir(path)
+    path = config.meme_path  
+    random_filename = random.choice([x for x in os.listdir(path)    # Get a random file from the directory
                                             if os.path.isfile(os.path.join(path, x))])
-    return os.path.join(path, random_filename)
+    return os.path.join(path, random_filename)      # Return the path, joined with the final destination
 
 # On startup, print this info
 @client.event
@@ -46,16 +52,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # Hello command
     if message.content.startswith('!hello'):
         msg = handleHello(message)
         await client.send_message(message.channel, msg)
 
+    # Quote command
     elif message.content.startswith('!quote'):
         msg = handleQuote()
         await client.send_message(message.channel, msg)
 
+    # Meme command
     elif message.content.startswith('!meme'):
-        fp = handleMeme()
+        fp = handleMeme()  # A file pointer is returned to the actual image
         print(fp)
         await client.send_file(message.channel, fp)
 
